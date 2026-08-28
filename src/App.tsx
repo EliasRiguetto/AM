811734b9831f87828fc7d59141e903fe253ae7e0
Elias Riguetto <eliasriguetto@gmail.com>
2026-08-28 09:56:49 -0300

    Add fonte/cores/container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import "./styles/variables.css";
+import "./styles/global.css";
+import { Container } from "./components/Container";
+
+
 export function App() {
   return (
     <>
-  
+  <Container>
+    <p>AMMM  Marq</p>
+  </Container>
     </>
   )
 }
